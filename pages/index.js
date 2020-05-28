a64fe1584c05d0ea8e091f879af4b38a90182e9e
Evgeny Timoshenko <timoshenko.evgeny@gmail.com>
2020-05-28 12:53:50 +0300

    1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import Header from '../components/header'
+import Header from "../components/header";
 
-const Home = () => (
+const Home = props => (
   <>
     <Header />
     <h1>Hello World!</h1>
+    <h3>{props.text}</h3>
   </>
-)
+);
 
-export default Home
+export default Home;
+
+export function getStaticProps() {
+  const props = import("../data.json").default;
+  console.log("props", props);
+  return {
+    props
+  };
+}
