77e1f88421891bb3b79d757714d37b4bc2817e23
Evgeny Timoshenko <timoshenko.evgeny@gmail.com>
2020-06-12 01:27:47 +0300

    bulma styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,35 @@
-import Link from 'next/link'
-import { readJsonSync } from 'fs-extra'
-import { resolve } from 'path'
+import Link from "next/link";
+import { readJsonSync } from "fs-extra";
+import { resolve } from "path";
+
+import "../src/styles.scss";
 
 const Home = ({ posts }) => (
   <>
-    {posts.map(({ slug, title }) => (
-      <div key={slug}>
-        <Link as={`/post/${slug}`} href="/post/[slug]">
-          <a>{title}</a>
-        </Link>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Section</h1>
+
+        <div className="container">
+          {posts.map(({ slug, title }) => (
+            <div key={slug}>
+              <Link as={`/post/${slug}`} href="/post/[slug]">
+                <a>{title}</a>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
+    </section>
   </>
 );
 
 export default Home;
 
 export async function getStaticProps() {
-  const posts = readJsonSync(resolve('data.json'))
+  const posts = readJsonSync(resolve("data.json"));
 
   return {
-    props: { posts }
+    props: { posts },
   };
 }
