784a94fa91634257df688978397794bc1491674d
Evgeny Timoshenko <timoshenko.evgeny@gmail.com>
2020-06-12 00:59:53 +0300

    using ts

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,25 +1,25 @@
-import { readJsonSync } from 'fs-extra'
-import { resolve } from 'path'
-import Link from 'next/link'
+import Link from "next/link";
 
-import { make } from '../../src/utils'
+import { make } from "../../src/utils";
 
 const Post = ({ title, text }) => {
   return (
     <>
       <i>
-        <Link href="/" as="/"><a>назад</a></Link>
+        <Link href="/" as="/">
+          <a>назад</a>
+        </Link>
       </i>
       <h1>{title}</h1>
       <main>{text}</main>
     </>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
 
-const { getStaticPaths, getStaticProps } = make()
-export { getStaticPaths, getStaticProps }
+const { getStaticPaths, getStaticProps } = make();
+export { getStaticPaths, getStaticProps };
 
 // export async function getStaticProps({ params }) {
 //   const posts = readJsonSync(resolve('data.json'))
